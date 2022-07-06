d2a95d791756884dffe9e4b181844b9d68c05e8d
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-07-06 08:49:05 -0300

    Adicionadas model Usuario, view LoginPage e viewModel LoginViewModel

---
+++ CheckPoint/CheckPoint/ViewModels/LoginViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using CheckPointBase.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CheckPoint.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private string email { get; set; }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string senha { get; set; }
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                senha = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Senha"));
+            }
+        }
+
+        public ICommand LoginCommand { protected set; get; }
+
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(Logar);
+
+        }
+
+        public void Logar()
+        {
+
+            var objUser = GetUsuarioByEmail(Email);
+            string emailBd = objUser.Email.ToString();
+            string senhaBd = objUser.Senha.ToString();
+            bool certo = false;
+            if (senha == senhaBd)
+            {
+                certo = true;
+            }
+            else
+            {
+                certo = false;
+            }
+        }
+
+
+        public List<Usuario> GetUsuarios()
+        {
+            return new List<Usuario>()
+            {
+                new Usuario(){ Id = 1, Email = "joao@email.com", Senha = "12345" },
+                new Usuario(){ Id = 2, Email = "pedro@email.com", Senha = "54321" },
+            };
+        }
+
+        public Usuario GetUsuarioByEmail(string email)
+        {
+            List<Usuario> _listaUsuarios = GetUsuarios();
+            var user = _listaUsuarios.Find(x => x.Email == email);
+
+            return user;
+        }
+    }
+}
--- CheckPointBase/CheckPointBase/App.xaml.cs
+++ CheckPointBase/CheckPointBase/App.xaml.cs
@@ -10,7 +10,7 @@ namespace CheckPointBase
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
         }
 
         protected override void OnStart()
