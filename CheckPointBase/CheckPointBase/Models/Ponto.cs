5036135647ee6e07f022d54ecf00e353f58882c4
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-07-06 14:34:12 -0300

    Adicionadas Models para Ponto e Relatório

---
+++ CheckPointBase/CheckPointBase/Models/Ponto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckPointBase.Models
+{
+    public class Ponto
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Local { get; set; }
+
+    }
+}
---
+++ CheckPointBase/CheckPointBase/Models/Relatorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckPointBase.Models
+{
+    public class Relatorio
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime TempoJornada { get; set; }
+        public DateTime Saldo { get; set; }
+        public string Status { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
